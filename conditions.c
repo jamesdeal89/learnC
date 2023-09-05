ee7ba6872309953cee437d530b1f6617a9cf7c49
James Deal <j891319@protonmail.com>
2023-09-05 16:02:01 +0800

    completed 3 more topics

--- conditions.c
+++ conditions.c
@@ -2,6 +2,13 @@
 
 void guessNumber(int guess) {
     // TODO: write your code here
+    if (guess == 555) {
+        printf("Correct. You guessed it!\n");
+    } else if (guess < 555) {
+        printf("your guess is too low.\n");
+    } else {
+        printf("Your guess is too high.\n");
+    }
 }
 
 int main() {
